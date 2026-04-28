4fe353038ac4a016cedd07a4342d298965fe84aa
Lenovo <Lenovo@LAPTOP-NOQLVM43>
2026-04-28 16:18:09 +0530

    Transaction module - deposit & withdraw APIs added

--- user-service/src/main/java/com/bank/user_service/entity/Account.java
+++ user-service/src/main/java/com/bank/user_service/entity/Account.java
@@ -3,10 +3,12 @@ package com.bank.user_service.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "account")
 public class Account {
     @Id
     @GeneratedValue
@@ -14,4 +16,23 @@ public class Account {
 
     private Long userId;
     private Double balance;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	
 }
